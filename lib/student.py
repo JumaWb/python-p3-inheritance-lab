b9f5bec42d91be98de827c53cedd44eba5975152
Brian Juma <bjuma0985@gmail.com>
2024-05-27 23:21:50 +0300

    inheritance_lab

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 from user import User
-
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name,last_name)
+        knowledge=[]
+        self.knowledge=knowledge
+
+    def learn(self,new_info):
+        self.knowledge=new_info
+
     
-    def learn(self):
-        pass
